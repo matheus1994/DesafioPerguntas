227df173c89e1c9105a63ea9b85996b4f30108ba
matheus1994 <31566239+matheus1994@users.noreply.github.com>
2024-05-16 11:31:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+function p3(){
+
+    s1 = document.getElementById("s1").value
+
+    if(s1 == "Sim"){
+        document.getElementById("div3").style.display = "block"
+    }
+    
+
+}
+
+function p4(){
+   
+    n1 = document.getElementById("n1").value
+
+    if(n1 == "Não"){
+        document.getElementById("div2").style.display = "block"
+    }
+    
+
+}
+
+
+function p5(){
+
+    s3 = document.getElementById("s3").value
+
+    if(s3 == "Sim"){
+        document.getElementById("div4").style.display = "block"
+    }
+    
+
+}
+
+function p6(){
+   
+    n1 = document.getElementById("n1").value
+
+    if(n1 == "Não"){
+        document.getElementById("div2").style.display = "block"
+    }
+    
+
+}
+
+function finaliza(){
+    alert("Finalizada com sucesso!")
+}
+
+
+
+
+
